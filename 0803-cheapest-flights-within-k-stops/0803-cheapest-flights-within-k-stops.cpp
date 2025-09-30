0328c4959ff3a73d40c963edfb5ea18cf68b62da
dixitshubham93 <161131809+dixitshubham93@users.noreply.github.com>
2025-09-30 22:53:04 +0530

    Time: 0 ms (100%), Space: 17.6 MB (90.04%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -6,36 +6,31 @@ public:
             adj[vec[0]].push_back({vec[1], vec[2]});
         }
 
-        vector<vector<int>> price(n, vector<int>(k+2, INT_MAX));
-        price[src][0] = 0;
+        vector<int> price(n, INT_MAX);
+        price[src] = 0;
 
-        priority_queue<pair<int , pair<int , int>>, 
-                       vector<pair<int , pair<int , int>>>, 
-                       greater<pair<int , pair<int , int>>>> q;
+        queue<pair<int,pair<int , int>>>q;
 
-        q.push({0, {src, 0}});
+        q.push({0, {0, src}});
 
         while(!q.empty()){
-            int node = q.top().second.first;
-            int tkt_p = q.top().first;
-            int k_count = q.top().second.second;
+            int stops = q.front().first;
+            int tkt_p = q.front().second.first;
+            int node = q.front().second.second;
             q.pop();
-
-            if(node == dst) return tkt_p;   
-
-            if(k_count == k+1) continue;   
+            if(stops == k+1) continue;   
 
             for(auto& adjecent : adj[node]){
                 int city = adjecent.first;
                 int tkt_p_c  = adjecent.second;
                 int newCost = tkt_p + tkt_p_c;
-
-                if(newCost < price[city][k_count+1]){
-                    price[city][k_count+1] = newCost;
-                    q.push({newCost, {city, k_count+1}});
+                if(newCost < price[city]&&stops+1<=k+1){
+                    price[city] = newCost;
+                    q.push({stops+1, {newCost,city}});
                 }
             }
         }
-        return -1;
+        if(price[dst]==INT_MAX)return -1;
+        return price[dst];
     }
 };
