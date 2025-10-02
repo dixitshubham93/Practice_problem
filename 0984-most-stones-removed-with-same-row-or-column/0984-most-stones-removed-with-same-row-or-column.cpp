4918517338302dcbe28c913f667659445d0d2614
dixitshubham93 <161131809+dixitshubham93@users.noreply.github.com>
2025-10-02 18:56:05 +0530

    Time: 16 ms (67.07%), Space: 24.7 MB (33.33%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
 
-   map<pair<int , int>,pair<int , int>>parent;
-   map<pair<int , int>,int>size;
-
- 
-   
-   pair<int , int> find(pair<int , int>x){
-       if(x == parent[x])return x;
-       return parent[x] = find(parent[x]);
+   vector<int>parent;
+    vector<int>size;
+   void DisjointSet(int V){
+       size.resize(V,1);
+       parent.resize(V,0);
+       for(int i=0;i<V;i++){
+           parent[i] = i;
+       }
+       return ;
    }
-  void Union(pair<int , int> x, pair<int ,int> y) {
-    pair<int , int> rootx = find(x);
-    pair<int  ,int> rooty = find(y);
+   
+   int find(int X){
+       if(X == parent[X])return X;
+       return parent[X] = find(parent[X]);
+   }
+  void Union(int x, int y) {
+    int rootx = find(x);
+    int rooty = find(y);
     if (rootx == rooty) return;
 
     if (size[rootx] > size[rooty]) {
@@ -24,36 +30,30 @@ public:
     }
 }
    
-    int Solve(int n, vector<vector<int>>& edges) {
-        
-         for(auto & edge : edges){
-            parent[{edge[0] , edge[1]}] = {edge[0],edge[1]};
-        }
-        for(auto & edge : edges){
-            size[{edge[0] , edge[1]}] = 1;
-        }
-
-        for(int i=0;i<n-1;i++){
-            for(int j =i+1;j<n;j++){
-                pair<int , int> pair1 = {edges[i][0],edges[i][1]};
-                pair<int , int> pair2 = {edges[j][0],edges[j][1]};
-                if(pair1.first==pair2.first||pair1.second==pair2.second){
-                    Union(pair1, pair2);
-                }
-            }
-        }
-        
-        
-        int ans = 0;
-        for (auto& it : parent) {
-    if (it.first == it.second) {  
-        ans += size[{it.first.first,it.first.second}] - 1;
-    }
-}
-        return ans;
-    }
     int removeStones(vector<vector<int>>& stones) {
         int n = stones.size();
-        return Solve( n , stones);
+       int maxRow = 0;
+       int maxCol = 0;
+       for(auto it : stones){
+        maxRow = max(maxRow , it[0]);
+        maxCol = max(maxCol , it[1]);
+       }
+       DisjointSet(maxRow + maxCol+2);
+       unordered_map<int , int>nodeStone;
+       for(auto it : stones){
+        int nodeRow = it[0];
+        int nodecol = maxRow+1+ it[1];
+        Union(nodeRow , nodecol);
+        nodeStone[nodeRow] = 1;
+        nodeStone[nodecol] = 1;
+       }
+
+       int count =0;
+       for(auto it : nodeStone){
+        if(parent[it.first]==it.first){
+            count++;
+        }
+       }
+      return n-count;
     }
 };
