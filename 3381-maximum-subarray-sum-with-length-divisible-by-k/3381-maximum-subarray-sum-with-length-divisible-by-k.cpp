f696c346d170427dde97e8456f8ebd81272721da
dixitshubham93 <161131809+dixitshubham93@users.noreply.github.com>
2025-11-30 13:51:33 +0530

    Time: 0 ms (100%), Space: 162.1 MB (92.07%) - LeetHub

--- 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
+++ 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -2,24 +2,18 @@ class Solution {
 public:
     long long maxSubarraySum(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<long long> minPrefix(k, LLONG_MAX);
-
+        vector<long long>minprefix(k ,LLONG_MAX);
+        minprefix[0]=0;
         long long prefix = 0;
-        long long answer = LLONG_MIN;
+        long long ans = LLONG_MIN;
 
-        minPrefix[0] = 0;
-
-        for (int i = 0; i < n; i++) {
-            prefix += nums[i];
-            int mod = (i + 1) % k;
-
-            if (minPrefix[mod] != LLONG_MAX) {
-                answer = max(answer, prefix - minPrefix[mod]);
+        for(int i=0;i<n;i++){
+            prefix+= nums[i];
+            if(minprefix[(i+1)%k]!=LLONG_MAX){
+            ans = max(ans , prefix - minprefix[(i+1)%k]);
             }
-
-            minPrefix[mod] = min(minPrefix[mod], prefix);
+            minprefix[(i+1)%k] = min(minprefix[(i+1)%k] , prefix);
         }
-
-        return answer;
+        return ans;
     }
 };
