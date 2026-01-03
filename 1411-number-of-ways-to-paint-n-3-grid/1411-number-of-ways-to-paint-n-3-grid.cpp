e30e6b196228b262cd291652c57fc322f50364ae
dixitshubham93 <161131809+dixitshubham93@users.noreply.github.com>
2026-01-03 09:02:32 +0530

    Time: 19 ms (39%), Space: 9.8 MB (42.67%) - LeetHub

--- 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-    int MOD = 1e9 + 7;
-    vector<vector<vector<vector<int>>>>arr;
-    int solve(int n, int f, int s, int t){
-        if (n == 0) return 1;   
+    static const int MOD = 1e9 + 7;
+    int arr[5001][4][4][4];
 
-        if(arr[n][f][s][t]!=-1)return arr[n][f][s][t];
+    Solution() {
+        memset(arr, -1, sizeof(arr)); 
+    }
 
-        int ans = 0;
+    int solve(int n, int f, int s, int t) {
+        if (n == 0) return 1;
+
+        int &res = arr[n][f][s][t];
+        if (res != -1) return res;
+
+        long long ans = 0;
 
         if (f == 0) {
             for (int i = 1; i <= 3; i++) {
                 for (int k = 1; k <= 3; k++) {
                     for (int j = 1; j <= 3; j++) {
                         if (j != i && j != k) {
-                            ans = (ans + solve(n - 1, i, j, k)) % MOD; 
+                            ans = (ans + solve(n - 1, i, j, k)) % MOD;
                         }
                     }
                 }
@@ -26,7 +32,7 @@ public:
                         if (k != t) {
                             for (int j = 1; j <= 3; j++) {
                                 if (j != i && j != k && j != s) {
-                                    ans = (ans + solve(n - 1, i, j, k)) % MOD; 
+                                    ans = (ans + solve(n - 1, i, j, k)) % MOD;
                                 }
                             }
                         }
@@ -34,12 +40,10 @@ public:
                 }
             }
         }
-        return arr[n][f][s][t] = ans;
+        return res = ans;
     }
 
     int numOfWays(int n) {
-        arr.assign(n+1 , vector<vector<vector<int>>>(4,vector<vector<int>>(4,vector<int>(4,-1))));
-        
         return solve(n, 0, 0, 0);
     }
 };
