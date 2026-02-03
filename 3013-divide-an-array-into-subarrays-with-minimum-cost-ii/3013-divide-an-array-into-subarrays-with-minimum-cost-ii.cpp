50b41b9e0b1fc2e00fc01acb54bc5f31f811ef0a
Shubham Dixit <161131809+dixitshubham93@users.noreply.github.com>
2026-02-03 09:55:53 +0530

    Time: 429 ms (41.03%), Space: 178.2 MB (39.49%) - LeetHub

--- 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.cpp
+++ 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.cpp
@@ -1,74 +1,66 @@
 class Solution {
 public:
-    struct SmartWindow{
-        int K;
-        multiset<int> low, high;
-        long long sumLow = 0;
-
-        SmartWindow(int k) : K(k) {}
-
-        int windowSize() const{
-            return (int)low.size() + (int)high.size();
-        }
-        void rebalance() {
-            int need = min(K, windowSize());
-
-            while((int)low.size() > need){
-                auto it = prev(low.end());
-                int x = *it;
-                low.erase(it);
-                sumLow -= x;
-                high.insert(x);
-            }
-            while((int)low.size() < need && !high.empty()){
-                auto it = high.begin();
-                int x = *it;
-                high.erase(it);
-                low.insert(x);
-                sumLow += x;
-            }
-        }
-        void add(int x){
-            if(low.empty() || x <= *prev(low.end())){
-                low.insert(x);
-                sumLow += x;
-            }
-            else{
-                high.insert(x);
-            }
-            rebalance();
-        }
-        void remove(int x){
-            auto itLow = low.find(x);
-            if(itLow != low.end()){
-                low.erase(itLow);
-                sumLow -= x;
-            }
-            else{
-                auto itHigh = high.find(x);
-                if(itHigh != high.end()){
-                    high.erase(itHigh);
-                }
-            }
-            rebalance();
-        }
-        long long query() const{
-            return sumLow;
-        }
-    };
     long long minimumCost(vector<int>& nums, int k, int dist) {
-        int n = (int)nums.size();
-        k -= 1;
-        SmartWindow window(k);
-        for(int i = 1; i <= 1 + dist; i ++){
-            window.add(nums[i]);
+        multiset<int> p1, p2;
+        long long sum = 0, mn = LLONG_MAX;
+        int n = nums.size();
+
+        int s = 1;
+        int e = min(n - 1, 1 + dist);
+
+       
+        for (int i = s; i <= e; i++) p2.insert(nums[i]);
+
+        
+        while (p1.size() < k - 1) {
+            auto it = p2.begin();
+            sum += *it;
+            p1.insert(*it);
+            p2.erase(it);
         }
-        long long ans = window.query();
-        for(int i = 2; i + dist < n; i ++){
-            window.remove(nums[i - 1]);
-            window.add(nums[i + dist]);
-            ans = min(ans, window.query());
+
+        mn = sum;
+
+        while (e + 1 < n) {
+            // slide window
+            int out = nums[s++];
+            int in = nums[++e];
+
+            // remove outgoing
+            if (auto it = p1.find(out); it != p1.end()) {
+                sum -= *it;
+                p1.erase(it);
+            } else {
+                auto it2 = p2.find(out);
+                if (it2 != p2.end()) p2.erase(it2);
+            }
+
+            // add incoming
+            p2.insert(in);
+
+            // rebalance
+            while (p1.size() < k - 1) {
+                auto it = p2.begin();
+                sum += *it;
+                p1.insert(*it);
+                p2.erase(it);
+            }
+
+            while (!p2.empty() && !p1.empty() && *p2.begin() < *p1.rbegin()) {
+                auto small = p2.begin();
+                auto large = prev(p1.end());
+
+                sum += *small - *large;
+                p1.insert(*small);
+                p2.insert(*large);
+
+                p1.erase(large);
+                p2.erase(small);
+            }
+
+            mn = min(mn, sum);
         }
-        return ans + nums[0];
+
+        return mn + nums[0];
     }
 };
