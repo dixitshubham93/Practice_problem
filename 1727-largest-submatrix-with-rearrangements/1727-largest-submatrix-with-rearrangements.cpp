d806c43122d41aad79a1941d1c4dad9405a69132
Shubham Dixit <161131809+dixitshubham93@users.noreply.github.com>
2026-03-17 09:35:41 +0530

    Time: 16 ms (88.83%), Space: 79.7 MB (96.28%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-  int solve(vector<int>&histo ){
-        int m = histo.size();
-        vector<int>temp(histo.begin() , histo.end());
-        sort(temp.begin() , temp.end());
-        int mx = 0;
-        for(int i = 0;i<m;i++){
-            mx = max(mx , temp[i]*(m-i));
-        }
-        return mx;
-    }
-
-    int largestSubmatrix(vector<vector<int>>& matrix){
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int mx = 0;
-        vector<int>histo(m);
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(matrix[i][j]==1){
-                    histo[j]++;
-                }else{
-                    histo[j] = 0;
-                }
+    int largestSubmatrix(vector<vector<int>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        for(int j = 0; j < m; j++){
+            int sum = 0;
+            for(int i = 0 ; i < n ; i++){
+                if(matrix[i][j] == 0 ) sum=0;
+                else{
+                    sum++;
+                    matrix[i][j] = sum;
+                } 
             }
-            mx = max(mx , solve(histo));
         }
-      return mx;
+        int res = 0;
+        for(int i = 0 ; i < n; i++){
+            sort(matrix[i].begin(), matrix[i].end(), greater<>());  
+            for(int j = 0 ; j < m; j++){    
+                res = max(res, (j+1)*matrix[i][j]);
+            }
+        }
+        return res;
     }
 };
