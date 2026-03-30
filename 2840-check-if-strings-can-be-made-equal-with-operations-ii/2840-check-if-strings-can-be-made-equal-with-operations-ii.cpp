47f4b40173c98c90a479253433fcea817e8512b0
Shubham Dixit <161131809+dixitshubham93@users.noreply.github.com>
2026-03-30 07:44:18 +0530

    Time: 4 ms (83.33%), Space: 19.4 MB (89.08%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -2,26 +2,22 @@ class Solution {
 public:
     bool checkStrings(string s1, string s2) {
 
-        vector<int>s1odd(26);
-        vector<int>s1even(26);
-        vector<int>s2odd(26);
-        vector<int>s2even(26);
+        vector<int>odd(26);
+        vector<int>even(26);
         int n = s1.size();
        for(int i = 0;i<n;i++){
         if(i%2){
-            s1odd[s1[i]-'a']++;
+            odd[s1[i]-'a']++;
+            odd[s2[i]-'a']--;
         }else{
-            s1even[s1[i]-'a']++;
+            even[s1[i]-'a']++;
+            even[s2[i]-'a']--;
         }
        }
-       for(int i = 0;i<n;i++){
-        if(i%2){
-            s2odd[s2[i]-'a']++;
-        }else{
-            s2even[s2[i]-'a']++;
-        }
+       for(int i = 0;i<26;i++){
+            if(odd[i]!=0||even[i]!=0){return false;}
        }
 
-        return s1odd==s2odd&&s1even==s2even;
+        return true;
     }
 };
