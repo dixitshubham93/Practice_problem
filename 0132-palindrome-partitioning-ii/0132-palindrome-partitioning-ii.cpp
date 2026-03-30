ab711bf67d94d20a0ab589bd0586a3d453811133
Shubham Dixit <161131809+dixitshubham93@users.noreply.github.com>
2026-03-30 17:21:02 +0530

    Time: 2989 ms (5.01%), Space: 759.8 MB (5.11%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,59 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    vector<vector<int>> pp;
-    
-
-    bool isPalin(string &s , int front , int end) {
-
-        while (front <= end) {
-            if (s[front] != s[end]) return false;
-            front++;
-            end--;
+   int dp[2001];
+    bool isPalin(string& s , int i , int j){
+        while(i<j){
+            if(s[i]!=s[j]){return false;}
+            i++;
+            j--;
         }
         return true;
     }
-
+    int minCost(string s  , int idx){
+        if(idx==s.size()){return 0;}
+        if(dp[idx]!=-1){return dp[idx];}
+        int mn = INT_MAX;
+        for(int i = idx;i<s.size();i++){
+            if(isPalin(s , idx , i)){
+                mn = min(mn , 1+minCost(s , i+1));
+            }
+        }
+        return dp[idx] = mn;
+    }
     int minCut(string s) {
-        int n = s.size();
-        dp = vector<vector<int>>(n + 1, vector<int>(n + 1, 0));
-        pp = vector<vector<int>>(n + 1, vector<int>(n + 1, 0));
-       for(int i=0;i<n;i++)
-       {
-        for(int j =0;j<n;j++)
-        {
-            pp[i][j] = isPalin(s,i,j);
-        }
-       }
-        
-        for (int i = 0; i < n; i++) {
-            if (pp[i][n - 1]) {
-                dp[i][n - 1] = 0;
-            } else {
-                dp[i][n - 1] = n - 1 - i;
-            }
-        }
-
-        // Bottom-up DP: i = front, j = end
-        for (int front = n - 1; front >= 0; front--) {
-            for (int end = n - 2; end >= front; end--) {
-                int ans = INT_MAX;
-
-                if (pp[front][end]) {
-                    int f = end + 1;
-                    int e = f;
-                    if (e >= n) {
-                        ans = 0;
-                    } else {
-                        ans = 1 + dp[f][e];
-                    }
-                }
-
-                ans = min(ans, dp[front][end + 1]);
-                dp[front][end] = ans;
-            }
-        }
-
-        return dp[0][0];
+        memset(dp , -1 , sizeof(dp));
+        return minCost(s , 0)-1;
     }
 };
