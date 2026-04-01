fb16ce5be94f95bdfa9e39f3ab62690170a4b219
Shubham Dixit <161131809+dixitshubham93@users.noreply.github.com>
2026-04-01 19:18:23 +0530

    Time: 3 ms (82.79%), Space: 30.5 MB (21.26%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -1,43 +1,23 @@
 class Solution {
 public:
-    int solve(vector<int>&histo , int k){
-        int n = histo.size();
-        int ans = 0;
-        for(int i = 1;i<=k;i++){
-            int count =0;
-            for(int j = 0;j<n;j++){
-
-                if(histo[j]>=i){
-                    count++;
-                }else{count=0;}
-
-                if(count==i){
-                    count--;
-                    ans++;
-                }
-            }
-        }
-        
-        return ans;
-    }
     int countSquares(vector<vector<int>>& matrix) {
-       
-        int n = matrix.size();
-        int m = matrix[0].size();
-        
-        vector<int>histo(m,0);
-
+        vector<vector<int>>dp = matrix;
+        int n  = matrix.size();
+        int m  = matrix[0].size();
         int ans = 0;
         for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){               
-             if(matrix[i][j]==1){
-                histo[j]++;
-             }else{
-                histo[j]=0;
-             }
+            for(int j  =0;j<m;j++){
+               if(dp[i][j]==1){
+                  if(i==0||j==0){
+                    ans++;
+                    continue;
+                  }
+                  dp[i][j]+= min(dp[i-1][j] , min(dp[i][j-1] , dp[i-1][j-1]));
+                  ans  += dp[i][j];
+               }
             }
-            ans += solve(histo , i+1);
         }
-      return ans;
+
+       return ans;
     }
 };
