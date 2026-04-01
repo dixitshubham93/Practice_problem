83e3b57db0ff1c8b1d7023f5153f621217976343
Shubham Dixit <161131809+dixitshubham93@users.noreply.github.com>
2026-04-02 00:16:34 +0530

    Time: 40 ms (93.69%), Space: 25 MB (92.39%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    unordered_map<int, unordered_set<int>> dp; // dp[index][k] => true if it's been tried and failed
-
-    bool solve(vector<int>& stones, int index, int k, unordered_map<int, int>& stoneIndex) {
-        if (index == stones.back()) return true;
-        if (dp[index].count(k)) return false;
-
-        for (int jump = k - 1; jump <= k + 1; ++jump) {
-            if (jump <= 0) continue;
-            int nextPos = index + jump;
-            if (stoneIndex.count(nextPos)) {
-                if (solve(stones, nextPos, jump, stoneIndex)) return true;
+    unordered_map<int , unordered_map<int , bool>>dp;
+    bool solve(vector<int>&stones,unordered_map<int , int>&mp , int idx , int k){
+        if(idx==stones.size()-1){return true;}
+        bool ans = false;
+        if(dp.count(idx)&&dp[idx].count(k)){return dp[idx][k];}
+        for(int i = k-1;i<=k+1;i++){
+            if(i<=0){continue;}
+            int cor = k+stones[idx];
+            if(mp.count(cor)){
+            if(solve(stones , mp , mp[cor] , i)){return true;}
             }
         }
-
-        dp[index].insert(k); // mark this state as failed
-        return false;
+        return dp[idx][k]=false;
     }
+    bool canCross(vector<int>& stones){
+        unordered_map<int , int>mp;
+        int n = stones.size();
 
-    bool canCross(vector<int>& stones) {
-        if (stones[1] != 1) return false;
-
-        unordered_map<int, int> stoneIndex;
-        for (int stone : stones) {
-            stoneIndex[stone] = 1;
+        for(int i = 0;i<n;i++){
+            mp[stones[i]] = i;
         }
 
-        return solve(stones, 1, 1, stoneIndex);
+      return solve(stones , mp , 0 , 0);
     }
 };
