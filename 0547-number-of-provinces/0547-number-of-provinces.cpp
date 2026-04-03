f8a7cb530b460d4ab28f8bd627f4dfd9f437bcaf
Shubham Dixit <161131809+dixitshubham93@users.noreply.github.com>
2026-04-03 19:29:50 +0530

    Time: 3 ms (39.66%), Space: 19.5 MB (73.63%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,35 +1,49 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& adj , vector<int>&visited , int idx){
-       visited[idx] = 1;
-       for(int i : adj[idx]){
-        if(visited[i]==0){
-            dfs(adj  , visited , i);
-        }
-       }
-       return ;
+   vector<int>size;
+   vector<int>parent;
+   void DSU(int n){
+    size.assign(n , 1);
+    parent.assign(n , 0);
+    for(int i = 0;i<n;i++){parent[i] = i;}
+   }
+   int find(int x){
+     if(x==parent[x])return x;
+     return parent[x] = find(parent[x]);
+   }
+   void add(int x  , int y){
+    int root_x = find(x);
+    int root_y = find(y);
+
+    if(root_x==root_y){return ;}
+
+    if(size[root_x]>size[root_y]){
+       size[root_x] += size[root_y];
+       parent[root_y] = root_x;
+    }else{
+       size[root_y] += size[root_x];
+       parent[root_x] = root_y;
     }
+   }
+
+
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<vector<int>> adj(n);
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (isConnected[i][j]) {
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+        DSU(n);
+        for(int i = 0 ;i<n;i++){
+            for(int j = 0;j<n;j++){
+                if(isConnected[i][j]){
+                    add(i , j);
                 }
             }
         }
-
-        vector<int>visited(n ,0);
+        
         int count = 0;
-        for(int i = 0;i<n;i++){
-            if(visited[i]==0){
+        for(int i= 0;i<n;i++){
+            if(i == parent[i]){
                 count++;
-                dfs(adj , visited , i);
             }
         }
-        return count;
+      return count;
     }
 };
