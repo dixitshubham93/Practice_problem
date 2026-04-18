136556859fc05c59021d25cc618423c354ba08ed
Shubham Dixit <161131809+dixitshubham93@users.noreply.github.com>
2026-04-18 07:37:29 +0530

    Time: 0 ms (100%), Space: 8.4 MB (86.21%) - LeetHub

---
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverse(int x){
+        int y = 0;
+        while(x){
+            y = y*10+x%10;
+            x = x/10;
+        }
+        return y;
+    }
+    int mirrorDistance(int n) {
+        return abs(n - reverse(n));
+    }
+};
