2e44f449b22999a4d8df3485ce48fea26eaaef8e
Shubham Dixit <161131809+dixitshubham93@users.noreply.github.com>
2026-04-18 17:07:05 +0530

    Time: 19 ms (98.2%), Space: 37.2 MB (95.27%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,56 +1,57 @@
 class Solution {
 public:
-     vector<int>parent;
-     vector<int>size;
-     void disjoinset(int v){
-        size.resize(v,1);
-        parent.resize(v,0);
-        for(int i=0;i<v;i++){
+    vector<int>parent;
+    vector<int>size;
+    void DSU(int x){
+        size.assign(x , 1);
+        parent.assign(x , 0);
+        for(int i = 0;i<x;i++){
             parent[i] = i;
         }
-     }
-     int find(int x){
+    }
+    int find(int x){
         if(parent[x]==x)return x;
         return parent[x] = find(parent[x]);
-     }
-     void unionset(int x , int y){
+    }
+    void Union(int x , int y){
         int rootx = find(x);
         int rooty = find(y);
         if(rootx==rooty)return ;
-        if(size[rootx]>size[y]){
+        if(size[rootx]>=size[rooty]){
             parent[rooty] = rootx;
-            size[rootx]+=rooty;
+            size[rootx] += size[rooty]; 
         }else{
             parent[rootx] = rooty;
-            size[rooty]+=rootx;
+            size[rooty] += size[rootx]; 
         }
-     }
+    }
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-       int n = accounts.size();
-       disjoinset(n);
-       unordered_map<string , int>emailparent;
-       for(int i=0;i<n;i++){
-        for(int j = 1;j<accounts[i].size();j++){
-           if(emailparent.find(accounts[i][j])!=emailparent.end()){
-            unionset(i , emailparent[accounts[i][j]]);
-           }else{
-            emailparent[accounts[i][j]] = i;
-           }
-        }
-       }
+        unordered_map<string , int>mp;
 
-       vector<vector<string>>merge(n);
-       for(auto& it : emailparent){
-        int node = find(it.second);
-        merge[node].push_back(it.first);
-       }
-       vector<vector<string>>ans;
-       for(int i=0;i<n;i++){
-        if(merge[i].empty())continue;
-        sort(merge[i].begin() , merge[i].end());
-        merge[i].insert(merge[i].begin() ,accounts[i][0] );
-        ans.push_back(merge[i]);
-       }
+        int n = accounts.size();
+        DSU(n);
+        for(int i = 0;i<n;i++){
+            int m = accounts[i].size();
+            for(int j = 1;j<m;j++){
+                if(mp.count(accounts[i][j])>0){
+                    Union(mp[accounts[i][j]],i );
+                }else{
+                  mp[accounts[i][j]] = i; 
+                }
+            }
+        }
+        vector<vector<string>>temp(n);
+        for(auto& it : mp){
+         int node = find(it.second);
+         temp[node].push_back(it.first);
+        }
+        vector<vector<string>>ans;
+         for(int i = 0;i<n;i++){
+        if(temp[i].size()==0){continue;}
+        sort(temp[i].begin() , temp[i].end());
+        temp[i].insert(temp[i].begin() , accounts[i][0]);
+        ans.push_back(temp[i]);
+        }
         return ans;
     }
 };
