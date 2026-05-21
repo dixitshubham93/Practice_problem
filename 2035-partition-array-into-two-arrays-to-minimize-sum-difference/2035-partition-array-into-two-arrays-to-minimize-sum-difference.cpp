dd96028c35213831dcee110c3cab0e40ddabb2c0
Shubham Dixit <161131809+dixitshubham93@users.noreply.github.com>
2026-05-21 08:39:43 +0530

    Time: 417 ms (44.68%), Space: 127.1 MB (8.36%) - LeetHub

--- 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
+++ 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -1,55 +1,49 @@
 class Solution {
 public:
     typedef long long ll;
-    ll solve(vector<ll>&arr1 , vector<ll>&arr2 , ll total){
-
-        int n = arr1.size();
-        int m = arr2.size();
+    ll solve(vector<ll>& part1 , vector<ll>&part2 , ll total){
         ll mindiff = LLONG_MAX;
+        for(int i = 0;i<part1.size();i++){
+            ll target = total/2-part1[i];
 
-        for(int i = 0;i<n;i++){
-            ll target = total/2 - arr1[i];
-            int lb = lower_bound(arr2.begin() , arr2.end() , target)-arr2.begin();
-        
+            int lb = lower_bound(part2.begin() , part2.end() , target)-part2.begin();
             if(lb>0){
-              mindiff  =min(mindiff , abs(total - 2*(arr1[i]+arr2[lb-1])));
+                mindiff = min(mindiff , abs(total-2*(part1[i]+part2[lb-1])));
             }
-            if(lb<m){
-             mindiff  =min(mindiff , abs(total - 2*(arr1[i]+arr2[lb])));
+            if(lb<part2.size()){
+                mindiff = min(mindiff , abs(total-2*(part1[i]+part2[lb])));
             }
-        }
-      return mindiff;
-    }
-    int minimumDifference(vector<int>& nums){
-
-        int n = nums.size();
-        int half = n/2;
-        vector<vector<ll>>res1(half+1);
-        vector<vector<ll>>res2(half+1);
-
-        for(int i = 0;i<(1<<half);i++){
-            ll sum1 = 0;
-            ll sum2 = 0;
-            int count = 0;
-            for(int mask = 0;mask<half;mask++){
-                if(i&(1<<mask)){
-                    count++;
-                    sum1+=nums[mask];
-                    sum2+=nums[mask+half];
-                }
-            }
-             res1[count].push_back(sum1);
-             res2[count].push_back(sum2);
-        }    
-
-        ll mindiff = LLONG_MAX;
-        ll total = accumulate(nums.begin() , nums.end() , 0LL);
-
-        for(int i = 0;i<=half;i++){
-            sort(res2[half-i].begin() , res2[half-i].end());
-
-            mindiff = min(mindiff , solve(res1[i] , res2[half-i] , total));
         }
         return mindiff;
+    }
+    int minimumDifference(vector<int>& nums){
+        int n = nums.size();
+        int half = n/2;
+
+        vector<vector<ll>>part1(half+ 1);
+        vector<vector<ll>>part2(half+ 1);
+
+        for(int i = 0;i<(1<<half);i++){
+            ll count = 0;
+            ll sum1 = 0;
+            ll sum2 = 0;
+          for(int mask = 0;mask<half;mask++){
+             if(i&(1<<mask)){
+               count++;
+               sum1 += nums[mask];
+               sum2 += nums[mask+half];
+             }
+           }
+           part1[count].push_back(sum1);
+           part2[count].push_back(sum2);
+        }
+
+        ll mindiff = LLONG_MAX;
+        ll total = accumulate(nums.begin() , nums.end(), 0LL);
+        for(int i = 0;i<=half;i++){
+          sort(part2[i].begin() , part2[i].end());
+          mindiff = min(mindiff , solve(part1[half-i] , part2[i] ,total));
+        }
+      return mindiff;
     }
 };
