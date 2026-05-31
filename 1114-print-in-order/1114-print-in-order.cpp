cad9e768ed500ceb4fe20d0b7ba0e2c168c238f6
Shubham Dixit <161131809+dixitshubham93@users.noreply.github.com>
2026-05-31 18:33:02 +0530

    Time: 4 ms (70.21%), Space: 9.5 MB (76.43%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -0,0 +1,34 @@
+class Foo {
+private:
+    int turn;
+    condition_variable cv;
+    mutex mtx;
+public:
+    Foo() {
+        turn = 1;
+    }
+    void first(function<void()> printFirst) {
+             
+        printFirst();
+        turn = 2;
+        cv.notify_all();
+    }
+
+    void second(function<void()> printSecond) {
+        unique_lock<mutex>lock(mtx);
+        while(turn!=2){
+            cv.wait(lock);
+        }
+        printSecond();
+        turn = 3;
+        cv.notify_all();
+    }
+
+    void third(function<void()> printThird) {
+      unique_lock<mutex>lock(mtx);
+       while(turn!=3){
+        cv.wait((lock));
+       } 
+        printThird();
+    }
+};
