96d9cbc5022e5569e37d8669f6fc07b5baa6d49e
Shubham Dixit <161131809+dixitshubham93@users.noreply.github.com>
2026-06-09 16:49:26 +0530

    Time: 12 ms (51.81%), Space: 35 MB (52.33%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,40 +1,34 @@
 class Solution {
 public:
-    bool check(int mid, int days, vector<int>& weights) {
-        int daysNeeded = 1; 
-        int currentLoad = 0;
-        
-        for (int w : weights) {
-            if (currentLoad + w > mid) {
-                daysNeeded++;     
-                currentLoad = w;   
-            } else {
-                currentLoad += w;
-            }
-        }
-        
-        return daysNeeded <= days;
+    bool check(int x , int days){
+        return x>days;
     }
 
     int shipWithinDays(vector<int>& weights, int days) {
-       
-        int l = *max_element(weights.begin(), weights.end());
-      
-        int r = 0;
-        for (int w : weights) r += w;
-        
-        int ans = r;
-        
-        while (l <= r) {
-            int mid = l + (r - l) / 2; 
+        int l = *max_element(weights.begin() , weights.end()) - 1;
+        int sum = accumulate(weights.begin() , weights.end() , 0);
+        int r = sum;
+
+        while(l+1<r){
+            int mid = l + (r-l)/2;
             
-            if (check(mid, days, weights)) {
-                ans = mid;     
-                r = mid - 1;
-            } else {
-                l = mid + 1;   
+            int x = 0;
+            int temp = 0;
+            for(int i = 0;i<weights.size();i++){
+               if(temp+weights[i]>mid){
+                  temp = weights[i];
+                  x++;
+               }else{
+                temp += weights[i];
+               }
+            }
+            if(temp>0)x++;
+            if(check(x , days)){
+                l = mid;
+            }else{
+                r = mid;
             }
         }
-        return ans;
+        return r;
     }
 };
