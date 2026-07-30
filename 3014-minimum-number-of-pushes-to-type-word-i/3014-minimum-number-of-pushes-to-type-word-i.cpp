a937152c2ebc6a0f3c76a1382480d89e401c4674
Shubham Dixit <161131809+dixitshubham93@users.noreply.github.com>
2026-07-30 06:14:40 +0530

    Time: 0 ms (100%), Space: 8.4 MB (54.67%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        unordered_map<char , int>mp;
-        for(int i = 0;i<word.size();i++){
-            if(mp.count(word[i])==0){
-                mp[word[i]] = (mp.size())/8+1;
-            }
-        }
-        int ans  = 0;
-        for(int i = 0;i<word.size();i++){
-            ans += mp[word[i]];
-        }
-        return ans;
+        int n  = word.size()/8;
+        int r  = word.size()%8;
+        return n*(8 + 4*(n-1)) + r*(n+1);
     }
 };
